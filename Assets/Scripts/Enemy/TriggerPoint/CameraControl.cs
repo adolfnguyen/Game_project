ec3f8938f78640ce166f087f6b4a42e02d6615d5
BaoTrungVuong <shaidokaa@gmail.com>
2021-07-06 23:30:24 +0700

    Gần như chả thêm gì :v

--- Assets/Scripts/Enemy/TriggerPoint/CameraControl.cs
+++ Assets/Scripts/Enemy/TriggerPoint/CameraControl.cs
@@ -4,23 +4,37 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-
+    Camera cam;
+    Vector2 m_velo;
+    Vector3 m_desirePos = new Vector3(51.48f, 12.44f, -10f);
+    bool hehe = false;
     // Start is called before the first frame update
     void Start()
     {
-
+        cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        m_velo = cam.GetComponent<Camerafollow>().velocity;
+    }
+    private void Update()
+    {
+        if (hehe)
+        {
+            StartCoroutine(MovetoPos());
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            //camera.maxpos.x = 51.48f;
-            //camera.minpos.x = 51.48f;
-            //camera.maxpos.y = 12.44f;
-            //camera.minpos.y = 12.44f;
-            //Camera.main.gameObject.transform.position = new Vector3(51.48f, 12.44f, -10.0f);
-            Camera.main.gameObject.GetComponent<Camerafollow>().maxpos = new Vector2(51.48f, 12.44f);
-            Camera.main.gameObject.GetComponent<Camerafollow>().minpos = new Vector2(51.48f, 12.44f);
+            //StartCoroutine(MovetoPos());
+            hehe = true;
         }
+    }
+    IEnumerator MovetoPos()
+    {        
+        float posx = Mathf.SmoothDamp(cam.transform.position.x, 51.48f, ref m_velo.x, 2.0f);
+        float posy = Mathf.SmoothDamp(cam.transform.position.y, 12.44f, ref m_velo.y, 2.0f);
+        cam.transform.position = new Vector3(posx, posy, transform.position.z);
+        yield return new WaitForSeconds(0.1f);
+        cam.GetComponent<Camerafollow>().SetActiveState(false);
     }
 }
